df99eb5ba08c329293d44c9423938c4045fb63f2
Adam Becvar <becvar.adam666@gmail.com>
2018-02-02 08:56:40 +0100

    Moved IO control to separate class

---
+++ src/rpn/CalculatorIOController.java
@@ -0,0 +1,41 @@
+package rpn;
+
+import rpn.operations.Operation;
+import rpn.parser.Parser;
+import rpn.parser.ParserException;
+import utility.PathStack;
+import utility.Stack;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class CalculatorIOController {
+    private RPNCalculator calculator;
+    private Parser parser;
+    private Scanner scanner;
+
+    public CalculatorIOController(RPNCalculator calculator, Parser parser, Scanner scanner) {
+        this.calculator = calculator;
+        this.parser = parser;
+        this.scanner = scanner;
+    }
+
+    public void start() throws ParserException {
+        Stack<Integer> numbersStack = new PathStack<>();
+        List<Operation> operations = new ArrayList<>();
+        while (true) {
+            String consoleInput = scanner.nextLine();
+            if (consoleInput.equals("END")) {
+                break;
+            }
+            String[] rawArguments = consoleInput.split(" ");
+            int oldSize = numbersStack.size();
+            numbersStack = parser.parseNumbers(numbersStack, rawArguments);
+            int countOfAddedNumbers = numbersStack.size() - oldSize;
+            operations = parser.parseOperations(operations, countOfAddedNumbers, rawArguments);
+            numbersStack = calculator.calculate(numbersStack, operations);
+            System.out.println(numbersStack);
+        }
+    }
+}
--- src/rpn/Main.java
+++ src/rpn/Main.java
@@ -2,33 +2,17 @@ package rpn;
 
 import rpn.parser.Parser;
 import rpn.parser.ParserException;
-import rpn.operations.Operation;
-import utility.ArrayStack;
-import utility.Stack;
 
 import java.util.*;
 
 
 public class Main {
     public static void main(String[] args) throws ParserException {
-        Scanner in = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         Parser parser = new Parser();
         RPNCalculator calculator = new RPNCalculator();
-        Stack<Integer> numbersStack = new ArrayStack<>();
-        List<Operation> operations = new ArrayList<>();
-        while (true) {
-            String consoleInput = in.nextLine();
-            if (consoleInput.equals("END")) {
-                break;
-            }
-            String[] rawArguments = consoleInput.split(" ");
-            int oldSize = numbersStack.size();
-            numbersStack = parser.parseNumbers(numbersStack, rawArguments);
-            int countOfAddedNumbers = numbersStack.size() - oldSize;
-            operations = parser.parseOperations(operations, countOfAddedNumbers, rawArguments);
-            numbersStack = calculator.calculate(numbersStack,operations);
-            System.out.println(numbersStack);
-        }
+        CalculatorIOController controller = new CalculatorIOController(calculator,parser,scanner);
+        controller.start();
     }
 
 
